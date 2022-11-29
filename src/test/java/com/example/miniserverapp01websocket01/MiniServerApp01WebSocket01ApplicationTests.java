4bb8ebe62e1c3f3d9062843e542018c4c9c25efb
Kota <maedakoutaa@gmail.com>
2022-11-29 21:47:48 +0900

    initial commit

---
+++ src/test/java/com/example/miniserverapp01websocket01/MiniServerApp01WebSocket01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.miniserverapp01websocket01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniServerApp01WebSocket01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
